812fef583356e34c744ff01214d5e9fefdf70f78
mitchellhill67 <mitchellhill67@gmail.com>
2022-06-13 23:50:05 +0100

    Updated with comments and no-duplicate-names guard

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,27 @@
   <div id="app">
     <vs-row>
     
-      <vs-col vs-w="7">
-        <vs-col vs-w="3" vs-offset="0.5" v-for="item in inventory" :key="item.name"
+    <!-- The "inventory card" element,
+         rendered for every item in the
+         'inventory' array in the script
+         tag. Vue.js loads these reactively,
+         taking away most of our updating
+         headaches. -->
+      <vs-col vs-w="7"> <!-- Just some clean gridspacing. -->
+      <!-- The v-for statement below tells Vue.js how to 
+           render (and re-render) card elements. We need 
+           to give it an object to iterate through and a key
+           to tell different iterations apart. -->
+        <vs-col vs-w="3" vs-offset="0.5" v-for="item in inventory" :key="item.name" 
                 vs-type="flex" vs-justify="left" vs-align="left">
-          <vs-card type="3">
+          <vs-card type="3"> <!-- <vs-card> is a third party (but very handy) component. -->
             Name: <b>{{item.name}}</b> <br>
             Description: <b>{{item.description}}</b> <br>
             Quantity: <b>{{item.quantity}}</b> <br>
             Price: <b>${{item.price}}</b> <br>
             Assigned Location: <b>{{item.location}}</b> <br><br>
-            <vs-button color="danger" @click="deleteItem(item.name)">Delete Item</vs-button>
+            <!-- For reference, the @click="" statement runs a method defined in the script when an element is clicked. -->
+            <vs-button color="danger" @click="deleteItem(item.name)">Delete Item</vs-button> 
           </vs-card> 
         </vs-col>
       </vs-col>
@@ -40,7 +51,7 @@
            <br>
            <br>
 
-           <div v-if="selectedAction == 'Add Item'">
+           <div v-if="selectedAction == 'Add Item'"> <!-- v-if renders the element it's in if the statement provided in quotes is true. -->
              <vs-input v-model="newItem.name" placeholder="Name(required)" />
              <vs-input v-model="newItem.description" placeholder="Description" />
              <vs-input v-model="newItem.quantity" placeholder="Quantity" type="number"/>
@@ -89,9 +100,9 @@
 
 export default {
   name: 'App',
-  data() {
+  data() { // This is where global variables are stored.
     return {
-      inventory: [
+      inventory: [ // Inventory tracker- updated with mutator functions defined in 'methods'.
         {'name': "Item #1", 'description': "Description for Item #1", 'quantity': 0, 'price': 100, 'location': "Berlin"},
         {'name': "Item #2", 'description': "Description for Item #2", 'quantity': 1, 'price': 150, 'location': "Belfast"},
         {'name': "Item #3", 'description': "Description for Item #3", 'quantity': 1, 'price': 100, 'location': "Barcelona"}
@@ -108,17 +119,24 @@ export default {
     }
   },
   methods: {
-    updateActionCard(argument){
+    updateActionCard(argument){ // Updates the UI title and v-if statements as needed.
       this.selectedAction = argument;
     },
-    addNewItem(){
-      this.inventory.push(this.newItem);
-      this.newItem = {'name': "", 'description': "", 'quantity': 0, 'price': 0, 'location': ""};
+    addNewItem(){ // Searches through the array, ensuring no duplicates exist, before adding a new inventory item.
+      for (var i = 0; i < this.inventory.length; i++) {
+        if (this.inventory[i].name == this.newItem.name) {
+          this.selectedAction = "Error: please ensure item names are unique";
+          return 0;
+        } else { // Why use an if-else loop here when an if loop will do? JavaScript takes advantage of an early form of speculative execution (yes, like SPECTRE!), so this code will run much faster with an if-else statement.
+          this.inventory.push(this.newItem);
+          this.newItem = {'name': "", 'description': "", 'quantity': 0, 'price': 0, 'location': ""}; // Clears user input.
+        }
+      }
     },
     editItem(){
-      for (var i = 0; i < this.inventory.length; i++) {
-        if (this.inventory[i].name == this.itemToEdit) {
-          if (this.newItem.name != "") {
+      for (var i = 0; i < this.inventory.length; i++) { // Basic array search to find the correct item to edit.
+        if (this.inventory[i].name == this.itemToEdit) { 
+          if (this.newItem.name != "") { // The loops below only edit inventory data that the user has made changes to- existing data is unaffected.
             this.inventory[i].name = this.newItem.name;
           }
           if (this.newItem.description != "") {
@@ -140,6 +158,19 @@ export default {
       for (var i = 0; i < this.inventory.length; i++) {
         if (this.inventory[i].name == itemName) {
           if (i > 0) {  
+            /* 
+              Because of how Vue.js detects
+              data updates, we can't use a 
+              simple 'delete' statement here-
+              we have to use inbuilt array 
+              functions to keep things running
+              smoothly. However, splice doesn't 
+              work correctly when there's only
+              one element in the array, so if
+              the array length is one (ie the 
+              only valid array index is 0) 
+              then we use shift().
+            */
             this.inventory.splice(i, i);
           } else {
             this.inventory.shift()
@@ -149,10 +180,10 @@ export default {
     },
     addLocation() {
       this.warehouses.push(this.newWarehouse);
-      this.newWarehouse = {'location': ""};
+      this.newWarehouse = {'location': ""}; // Clears user input upon completion.
     },
     deleteLocation(location) {
-      for (var i = 0; i < this.warehouses.length; i++) {
+      for (var i = 0; i < this.warehouses.length; i++) { // Searches through the array for the right location to delete.
         if (this.warehouses[i].location == location) {
           if (i > 0) {  
             this.warehouses.splice(i, i);
@@ -161,7 +192,7 @@ export default {
           }
         }
       }
-      for (i = 0; i < this.inventory.length; i++) {
+      for (i = 0; i < this.inventory.length; i++) { // If a location is deleted that's used by inventory items, the location data is wiped from said items.
         if (this.inventory[i].location == location) {
           this.inventory[i].location = "";
         }
@@ -169,6 +200,7 @@ export default {
     }
   }
 }
+
 
 
 </script>
